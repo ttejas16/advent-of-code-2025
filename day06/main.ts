90e71a89c1025d94f0cbad80fde9530a32570763
ttejas16 <jagtaptejas128@gmail.com>
2025-12-06 20:07:01 +0530

    day 6 part 2

--- day06/main.ts
+++ day06/main.ts
@@ -33,7 +33,47 @@ function part1(operands: number[][], operators: string[]) {
     return result;
 }
 
+function part2(inputLines: string[]) {
+    const operators = inputLines[inputLines.length - 1].split(" ").filter(n => n.length > 0);
+    const calculationStrings = inputLines
+        .slice(0, inputLines.length - 1)
+        .map(s => s.concat(" ")); // add one traling space to end of each line
 
-// console.time()
+    let result = 0;
+    let currentOperatorIndex = 0;
+    let arr: number[] = [];
+
+    for (let i = 0; i < calculationStrings[0].length; i++) {
+
+        let columnarString = '';
+        for (let j = 0; j < calculationStrings.length; j++) {
+            columnarString = columnarString.concat(calculationStrings[j].charAt(i));
+        }
+
+        columnarString = columnarString.replaceAll(" ", "");
+
+        if (columnarString.length == 0) {
+            const currentOperator = operators[currentOperatorIndex];
+
+            const currentResult = arr.reduce((acc, c) => {
+                if (currentOperator == '+') {
+                    return acc + c;
+                }
+                return acc * c;
+            }, currentOperator == '+' ? 0 : 1);
+
+            result += currentResult;
+            arr = [];
+            currentOperatorIndex++;
+        } else {
+            arr.push(parseInt(columnarString));
+        }
+    }
+
+    return result;
+}
+
+console.time()
 // console.log(part1(parsedOperands, parsedOperators));
-// console.timeEnd()
+console.log(part2(lines));
+console.timeEnd()
